7cbf300f11b80addc364b519c2921aeb583c5f5f
Jeremy Cohen <cohen.jke@gmail.com>
2018-09-15 21:28:16 -0700

    gonna commit

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from flask import Flask
 
 app = Flask (__name__)
 
+# error handling
+
+app.config["DEBUG"] = True
+
 # use the decorator pattern to link the view function to a url
 
 @app.route("/")
@@ -16,7 +20,18 @@ app = Flask (__name__)
 # define the view using a function, which returns a string
 
 def hello_world():
-	return "Hello, World!"
+	return "Hello, Merle!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not Found", 404
 
 # start the development server using the run() method
 
